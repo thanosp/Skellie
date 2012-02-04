615ac53487a9a2c34d3906c22a79557835960cf6
Thanos <thanos@webclusive.com>
2012-02-04 10:05:38 +0100

    Commented out dependencies and default configuration. The purpose is to have something very basic and clean in functions. Left the actions as placeholders

--- functions.php
+++ functions.php
@@ -9,9 +9,9 @@ require_once 'inc/WPTemplate.php';
  */
 function stylesAndScripts()
 {
-    wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css');
-    wp_enqueue_script('modernizr', get_template_directory_uri() . '/js/libs/modernizr-2.0.6.min.js');
-    wp_enqueue_style('theme_specific_style', get_template_directory_uri() . '/css/style.css');
+    // wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css');
+    // wp_enqueue_script('modernizr', get_template_directory_uri() . '/js/libs/modernizr-2.0.6.min.js');
+    // wp_enqueue_style('theme_specific_style', get_template_directory_uri() . '/css/style.css');
 }
 add_action('wp_enqueue_scripts', 'stylesAndScripts');
 
@@ -37,15 +37,15 @@ add_filter('template_include', 'bootstrap', 10000);
 function setupTheme()
 {
     // This theme uses wp_nav_menu() in one location.
-    register_nav_menus(array('primary' => 'Main Menu', 'utilities' => 'Utility Menu'));
+    // register_nav_menus(array('primary' => 'Main Menu', 'utilities' => 'Utility Menu'));
    
-    register_sidebar(array('name' => 'Default Left', 'id' => 'default-sidebar-left'));
-    register_sidebar(array('name' => 'Default Right', 'id' => 'default-sidebar-right'));
+    // register_sidebar(array('name' => 'Default Left', 'id' => 'default-sidebar-left'));
+    // register_sidebar(array('name' => 'Default Right', 'id' => 'default-sidebar-right'));
    
     //thumbnail sizes
-    add_theme_support('post-thumbnails');
-    add_image_size('preview', 111, 72, true); // thumbnails for featured-style article blocks (cropped)
-    add_image_size('slider', 278, 164, true); // thumbnails for foto blocks (cropped)
+    // add_theme_support('post-thumbnails');
+    // add_image_size('preview', 111, 72, true); // thumbnails for featured-style article blocks (cropped)
+    // add_image_size('slider', 278, 164, true); // thumbnails for foto blocks (cropped)
 
 
 }
